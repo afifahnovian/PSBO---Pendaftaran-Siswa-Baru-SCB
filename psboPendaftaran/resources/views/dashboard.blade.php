bb8f269ca5279d7db49d9f30f962357fd9d26443
Arih Ghina Wiranti <ghina_wiranti@apps.ipb.ac.id>
2020-03-12 16:40:09 +0700

    Dashboard

---
+++ psboPendaftaran/resources/views/dashboard.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> PPDB Online SMP Cendekia BAZNAS </title>
+        <!-- Font -->
+        <link rel="stylesheet" type="text/css" href= "https://fonts.googleapis.com/css?family=Open+Sans">
+
+        <!-- Styles -->
+        <style>
+        html, body {
+                background-color: #fff;
+            }
+            .top-right-title {
+                position: absolute;
+                right: 0px;
+                left:55px;
+                top: 200px;
+            }
+            .top-right-link {
+                position: absolute;
+                right: 0px;
+                left:950px;
+                top: 10px;
+                padding-right: 0.75rem 100;
+            }
+            .title {
+                font-family: 'Open Sans';
+                font-style: normal;
+                font-weight: bold;
+                font-size: 50px;
+                line-height: 70px;
+                display: flex;
+                align-items: center;             
+            }
+            .link {
+                font-family: 'Open Sans';
+                font-style: normal;
+                font-weight: normal;
+                font-size: 25px;
+                line-height: 76px;
+                align-items: right;  
+            }
+            .content {
+                text-align: left;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            .image {
+                position: absolute;
+                width: 237.43px;
+                height: 64px;
+                left: 35px;
+                top: 35px;
+            }
+        </style>
+    </head>
+    <body>
+        <body id="page-top">
+            <div class="image"><img src="{{url('/images/logo.png')}}" style= "width: 80%; height: 80%"></div>
+            <div class="top-right-link">                    
+                <a class="link" href="#tentangSCB" style="padding-right: 20px">Tentang SCB</a>                  
+                <a class="link" href="#kontak">Kontak</a>
+            </div>
+                <div class="top-right-title">
+                   <div class="title m-b-md">
+                        Penerimaan<br>
+                        Peserta Didik Baru<br>
+                        SMP Cendekia Baznas</br></br>
+                    </div>
+                </div>
+        </body>
+    </body>
+</html>
