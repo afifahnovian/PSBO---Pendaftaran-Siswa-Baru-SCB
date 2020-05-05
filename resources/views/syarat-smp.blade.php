3d4669ce8df9e00b958998372c3f7049186cdac8
Arih Ghina Wiranti <ghina_wiranti@apps.ipb.ac.id>
2020-05-06 00:59:06 +0700

    landing page, syarat
    +landing page
    +page syarat smp
    +page syarat tahfiz

---
+++ resources/views/syarat-smp.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Syarat Calon Peserta Dididk Baru SMP Sekolah Cendekia BAZNAS</title>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <img class="img-responsive" src="assets/img/logo-putih.png" alt="logo" style="width:200px;" /></a>
+                <button class="navbar-toggler navbar-toggler-right bg-primary rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="color: #004445 fas fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#beranda">Beranda</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Tentang SCB</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Kontak</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-black text-left">
+            <div class="container d-flex align-items-left flex-column">
+                <h1 class="masthead-heading mb-0">Syarat Calon Peserta Didik Baru Siswa SMP Sekolah Cendekia BAZNAS</h1><br><br><br>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Persyaratan Umum</p>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- persyaratan-->
+                <ol class="masthead-subsubheading font-weight-light mb-0 ml-4">
+                    <li>Beragama Islam. </li>
+                    <li>Berasal dari keluarga dhuafa atau tidak mampu. </li>
+                    <li>Lulus SD/Sederajat, usia maksimal 14 tahun pada 31 Juli 2020. </li>
+                    <li>Berbadan sehat dan tidak memiliki penyakit menular dan melampirkan surat dari dokter. </li>
+                    <li>Bersedia mengikuti seluruh tahapan seleksi sesuai dengan ketentuan yang berlaku. </li>
+                    <li>Tidak memiliki anggota keluarga yang sedang atau pernah mendapatkan beasiswa di Sekolah Cendekia BAZNAS. </li>
+                    <li>Tidak mengundurkan diri saat dinyatakan lolos seleksi.</li>
+                </ol>
+                <br>
+                <br>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Persyaratan Khusus</p>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- persyaratan-->
+                
+                    <ol class="masthead-subsubheading font-weight-light mb-0 ml-4">
+                    <li>Memiliki prestasi akademik dengan rata-rata nilai rapor kelas IV-V minimal 7.5 pada mata 
+                    pelajaran; Bahasa Indonesia, Ilmu Pengetahuan Alam (IPA), Matematika.</li> 
+                    <li>Mengisi formulir pendaftaran. </li>
+                    <li>Siap bertempat tinggal di asrama. </li>
+                    <li>Mengumpulkan berkas: </li>
+                    <ul class="masthead-subsubheading font-weight-light mb-0 ml-4" style="list-style-type:circle;">
+                    <li>Fotokopi kartu keluarga. </li>
+                    <li>Fotokopi rapor kelas IV hingga V yang telah dilegalisasi oleh pihak sekolah asal. </li>
+                    <li>Fotokopi Surat Keterangan Tidak Mampu (SKTM), fotokopi piagam atau sertifikat prestasi yang pernah di raih (bila ada).</li>
+                    </ul>
+                    </ol>
+                    <br>
+                    <br>
+                    <br>
+                <!--Narahubung-->
+                <p class="masthead-subsubheading font-weight-light mb-0">Narahubung :</p><br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4-2">
+                            <div class="container-2">
+                                <div class="row-2">
+                                    <div class="col-3">
+                                        <a class="btn1 btn-social mx-3 mb-2"><i class="fas fa-fw fa-phone-alt" style ="color:#ffffff;" ></i></a>
+                                    </div>
+                                    <div class="col-9">
+                                        <span class="masthead-subsubsubheading font-weight-light mb-0">Fatur (0852 8142 8912)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container-2">
+                                <div class="row-2">
+                                    <div class="col-3">
+                                        <a class="btn1 btn-social mx-3 mb-2"><i class="fas fa-fw fa-phone-alt" style ="color:#ffffff;"></i></a>
+                                    </div>
+                                    <div class="col-9">
+                                        <span class="masthead-subsubsubheading font-weight-light mb-0">Suci (0857 8250 7617)</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Narahubung-->
+                    <div class="col-lg-4-2 mb-5 mb-lg-0 container">
+                        <h4 style ="color:#f8b400;">Narahubung</h4><br>
+                        <footer class="text-left">
+                            <ul style="list-style-type:none;">
+                                <li><a class="btn1 btn-social mx-3 mb-2"><i class="fas fa-fw fa-phone-alt" style ="color:#004445;" ></i></a><span class="lead mb-0" style="font-size:110%;">(0521) 8591072</span></li>
+                                <li><a class="btn1 btn-social mx-3 mb-2"><i class="fas fa-fw fa-phone-alt" style ="color:#004445;"></i></a><span class="lead mb-0" style="font-size:110%;">0857 8277 1555 (Norma)</span></li>
+                                <li><a class="btn1 btn-social mx-3 mb-2"><i class="fas fa-fw fa-phone-alt" style ="color:#004445;"></i></a><span class="lead mb-0" style="font-size:110%;">0811 1372 017 (Aiman)</span></li>
+                            </ul>
+                        </nav>
+                        </footer>
+                    </div>
+                    <!-- Footer Sosisal media-->
+                    <div class="col-lg-4-2 mb-5 mb-lg-0 container">
+                        <h4 style ="color:#f8b400;">Sosial Media</h4><br>
+                        <div class="row" style="display:block;margin-left:auto;margin-right:auto;width:100%">
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fas fa-fw fa-globe" style ="color:#f8b400;"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-twitter" style ="color:#f8b400;"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-line" style ="color:#f8b400;"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-facebook-f" style ="color:#f8b400;"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fab fa-fw fa-instagram" style ="color:#f8b400;"></i></a>
+                        </div>
+                    </div>
+                    <!-- Footer Informasi Sekolah-->
+                    <div class="col-lg-4-2">
+                        <h4 style ="color:#f8b400;">Informasi Sekolah</h4><br>
+                        <p class="lead mb-0" style="font-size:110%;">SMP Cendekia BAZNAS<br> Jln. K.H. Umar Cirangkong, RT. 18 RW. 07, Ds. Cemplang, Kec. Cibungbulang, Kab. Bogor 16630</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+        <div class="scroll-to-top d-lg-none position-fixed">
+            <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
